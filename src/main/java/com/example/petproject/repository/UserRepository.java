b998b2e9b45943b5dc5e69e25b254f724f80337a
tshur <tarasuchok@ukr.net>
2022-09-24 23:07:12 +0300

    add userrepository and jenkins config

---
+++ src/main/java/com/example/petproject/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.petproject.repository;
+
+import com.example.petproject.model.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    List<User> findByLastName(@Param("name") String name);
+
+}
